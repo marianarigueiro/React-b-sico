5c800d5f4253eb1fdb417dec8eb1da278c77908d
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2025-09-07 15:13:05 -0300

    Add files via upload

---
+++ tela.js
@@ -0,0 +1,12 @@
+import "./tela.css";
+
+function Tela() {
+  return (
+    <div className="tela">
+        <h2>Essa é a Tela 2</h2>
+        <p>{param.children}</p>
+    </div>
+  );
+}
+
+export default Tela;
